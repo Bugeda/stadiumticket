1d4a94457561eac356b2ea9f4c27692ca1733e54
Denis Shchelkonogov <delis91@gmail.com>
2014-06-15 18:26:11 +0300

    Configuration changes

--- src/main/java/com/dataartschool2/stadiumticket/dreamteam/web/HelloController.java
+++ src/main/java/com/dataartschool2/stadiumticket/dreamteam/web/HelloController.java
@@ -2,9 +2,10 @@ package com.dataartschool2.stadiumticket.dreamteam.web;
 
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@Controller
 public class HelloController{
 
 	@RequestMapping("/index")
