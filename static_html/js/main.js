acc7c05d1500fc9d2e09277d2c0a87849105b819
Valentin Munitsa <valentin.mu@gmail.com>
2014-06-28 14:29:15 +0300

    copying data from sector plan to hidden main form fields, to send them to server

--- static_html/js/main.js
+++ static_html/js/main.js
@@ -2,6 +2,23 @@ $(document).ready(function () {
     if ($('.map').length != 0) {
 	$('.map').maphilight();
     };
+
+    // $('.close').click( function() { $(this).hide();} );
+
+    $('map > input').change( function () {
+	var source_id = $(this).attr('id');
+	var source_content = $(this).val();
+
+	// var reg = /^\d+$/;
+	// if (!reg.test(source_content)) { $(".alert").show();}
+
+	if ( source_id.substr(6) == 'vipA' || source_id.substr(6) == 'vipD' ) {
+	    var dest_name = source_id.substr(6);
+	}
+	else var dest_name = 's'+ source_id.substr(6);
+	$('#'+ dest_name).val(source_content);
+    });
+
     $('#event_list').dataTable({
 	"paging": false,
 	"stateSave": true,
@@ -14,5 +31,4 @@ $(document).ready(function () {
     $('.event').mouseout( function() {
     	$(this).children('.action_list').hide();
     });
-
 });
