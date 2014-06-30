ecd3e82cf0a8b8aa8c66843c9d9037cdb7fd78f6
Valentin Munitsa <valentin.mu@gmail.com>
2014-06-30 16:34:44 +0300

    added event delete confirmation dialog, minor cleanups and fixes

--- static_html/js/main.js
+++ static_html/js/main.js
@@ -1,33 +1,34 @@
 $(document).ready(function () {
+    // load maphiglight if <map> block present
     if ($('.map').length != 0) {
 	$('.map').maphilight();
+    }
 
-	$('map > input').change( function () {
-	    var source_id = $(this).attr('id');
-	    var source_content = $(this).val();
+    // copy data from sector plan to hidden form, in order to send it to server
+    $('map > input').change( function () {
+	var source_id = $(this).attr('id');
+	var source_content = $(this).val();
 
-	    // var reg = /^\d+$/;
-	    // if (!reg.test(source_content)) { $(".alert").show();}
+	// var reg = /^\d+$/;
+	// if (!reg.test(source_content)) { $(".alert").show();}
 
-	    if ( source_id.substr(6) == 'vipA' || source_id.substr(6) == 'vipD' ) {
-		var dest_name = source_id.substr(6);
-	    }
-	    else var dest_name = 's'+ source_id.substr(6);
-	    $('#'+ dest_name).val(source_content);
-	});
-
-    };
+	if ( source_id.substr(6) == 'vipA' || source_id.substr(6) == 'vipD' ) {
+	    var dest_name = source_id.substr(6);
+	}
+	else var dest_name = 's'+ source_id.substr(6);
+	$('#'+ dest_name).val(source_content);
+    });
 
     if ($('#event_list').length != 0) {
 
-	// $('.close').click( function() { $(this).hide();} );
-
+	// dataTable configuration
 	$('#event_list').dataTable({
 	    "paging": false,
 	    "stateSave": true,
 	    "autoWidth": true,
 	    "columnDefs": [ { "orderable": false, "targets": 2 } ]
 	});
+	// display action buttons for event in list
 	$('.event').mouseover( function() {
 	    $(this).children('.action_list').show();
 	});
@@ -36,4 +37,17 @@ $(document).ready(function () {
 	});
 	$('#event_list_filter input').addClass('form-control');
     };
+    // delete event process
+    $('#event_delete').click( function () {
+	$('#confirm_deletion_form').show();
+    });
+    $('#confirm_deletion_form').submit( function (event) {
+	if ($('#confirm_deletion_text').val() != 'DELETE') {
+	    $('#wrong_confirmation').show();
+	    event.preventDefault();
+	};
+    });
+    $('#cancel_deletion').click( function () {
+	$('#confirm_deletion_warning').hide();
+    });
 });
