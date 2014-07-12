f8be26bfca4b05a0a17ff6954b6df64c0611e1e0
DeLiS <delis91@gmail.com>
2014-07-13 01:09:16 +0300

    Integration

--- src/main/java/com/dataartschool2/stadiumticket/dreamteam/service/TicketService.java
+++ src/main/java/com/dataartschool2/stadiumticket/dreamteam/service/TicketService.java
@@ -1,6 +1,14 @@
 package com.dataartschool2.stadiumticket.dreamteam.service;
 
 
+import com.dataartschool2.stadiumticket.dreamteam.domain.Event;
+import com.dataartschool2.stadiumticket.dreamteam.domain.Seat;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface TicketService {
-		
+
+    void sellTickets(Event event, List<Seat> chosenSeats);
 }
--- src/main/java/com/dataartschool2/stadiumticket/dreamteam/service/TicketServiceImpl.java
+++ src/main/java/com/dataartschool2/stadiumticket/dreamteam/service/TicketServiceImpl.java
@@ -1,6 +1,34 @@
 package com.dataartschool2.stadiumticket.dreamteam.service;
 
 
+import com.dataartschool2.stadiumticket.dreamteam.dao.TicketDAO;
+import com.dataartschool2.stadiumticket.dreamteam.domain.Event;
+import com.dataartschool2.stadiumticket.dreamteam.domain.Seat;
+import com.dataartschool2.stadiumticket.dreamteam.domain.Ticket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Objects;
+@Component
 public class TicketServiceImpl implements TicketService {
-		
+
+    @Autowired
+    private TicketDAO ticketDAO;
+
+    @Override
+    public void sellTickets(Event event, List<Seat> chosenSeats) {
+        for(Seat seat : chosenSeats){
+            Ticket ticket = new Ticket();
+            ticket.setEvent(event);
+            ticket.setSeat(seat);
+            String ticketNumber = generateTicketNumber(event, seat);
+            ticket.setTicketNumber(ticketNumber);
+            ticketDAO.updateEntity(ticket);
+        }
+    }
+
+    private String generateTicketNumber(Event event, Seat seat) {
+        return Integer.toString(Objects.hash(event, seat));
+    }
 }
